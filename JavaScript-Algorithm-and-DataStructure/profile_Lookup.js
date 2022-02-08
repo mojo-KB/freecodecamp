6e690d6f37cd2d8684d69fb5957cca0762d14d02
mojo-KB <56409448+mojo-KB@users.noreply.github.com>
2022-02-08 00:10:33 -0500

    2/8/2022

---
+++ JavaScript-Algorithm-and-DataStructure/profile_Lookup.js
@@ -0,0 +1,36 @@
+// Setup
+const contacts = [{
+        firstName: "Akira",
+        lastName: "Laine",
+        number: "0543236543",
+        likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+        firstName: "Harry",
+        lastName: "Potter",
+        number: "0994372684",
+        likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+        firstName: "Sherlock",
+        lastName: "Holmes",
+        number: "0487345643",
+        likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+        firstName: "Kristian",
+        lastName: "Vos",
+        number: "unknown",
+        likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+];
+
+function lookUpProfile(name, prop) {
+    // Only change code below this line
+    if (contacts.firstName == name || contacts.hasOwnProperty(prop)) {
+        return contacts.prop;
+    }
+    // Only change code above this line
+}
+
+console.log(lookUpProfile("Akira", "likes"));
